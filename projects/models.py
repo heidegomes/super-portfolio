df96e64520a158c9daa2d04d01672818ab0f6f8f
heidegomes <heidegc@hotmail.com>
2023-11-07 17:49:20 -0300

    Requisito 1 feito!

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Profile(models.Model):
-    name = models.CharField(max_length=50, blank=False, min_length=500)
-    github = models.URLField(max_length=100, blank=False, min_length=500)
-    linkedin = models.URLField(max_length=100, blank=False, min_length=500)
-    bio = models.TextField(blank=False, min_length=500)
-
-    def string(self):
-        return self.name
