453e40b1f3a269461f630124d77f9e34cd7dce70
heidegomes <heidegc@hotmail.com>
2023-11-07 19:00:21 -0300

    Requisito 2 feito!

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    github = models.URLField(max_length=100, blank=False)
+    linkedin = models.URLField(max_length=100, blank=False)
+    bio = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from projects.models import Profile
+from projects.serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfilesListViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+# class CreateProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.create()
+#     serializer_class = ProfileSerializer
+
+
+# class IdProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.get(id=id)
+#     serializer_class = ProfileSerializer
+
+
+# class UpdatedProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.update(id=id)
+#     serializer_class = ProfileSerializer
+
+
+# class DeleteProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.delete(id=id)
+#     serializer_class = ProfileSerializer
